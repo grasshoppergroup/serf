b3a026b87610d18e2d9e7c5171581824375e3359
BruceCutler <bicutler10@gmail.com>
2017-08-08 10:43:36 -0400

    GDE-1161 Updating default.rb attribute file to properly reference the kernel machine attribute

--- metadata.rb
+++ metadata.rb
@@ -5,10 +5,11 @@ license          'The MIT License (MIT)'
 description      'Installs/Configures serf'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
+chef_version '>= 12.1' if respond_to?(:chef_version)
+version          '1.3.0'
+
 %w{ ubuntu centos }.each do |os|
   supports os
 end
 
 depends 'logrotate'
-
-version          '1.2.0'
